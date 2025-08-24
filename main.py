a100b356dcac0fde0c54671930dcd29b61ef5594
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2025-08-24 14:04:40 -0600

    learning security for fast api

--- main.py
+++ main.py
@@ -3,17 +3,18 @@
 This module defines the API endpoints and related logic.
 """
 
+# Import Annotated for type hinting with metadata in FastAPI endpoints
 from typing import Annotated
-from fastapi import FastAPI, Query
 
-app = FastAPI()
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
+
+app = FastAPI(description="This is a simple API to test the FastAPI framework")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.get("/items/")
-async def read_items(
-    hidden_query: Annotated[str | None, Query(include_in_schema=False)] = None,
-):
-    if hidden_query:
-        return {"hidden_query": hidden_query}
-    else:
-        return {"hidden_query": "Not found"}
+async def read_items(token: Annotated[str, Depends[oauth2_scheme]]):
+    """Get items from the API"""
+    return {"token": token}
